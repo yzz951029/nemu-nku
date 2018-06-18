54ce20446d5d9d627fd2440b63ad0934dfc48fd2
yzz951029 <747286218@qq.com>
2018-06-17 18:07:11 -0700

    pa2 step3 finished.

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +13,24 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int a = is_mmio(addr) ;
+  if(a == -1)
+  {
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  }
+  else
+  {
+     return mmio_read(addr, len, a) & (~0u >> ((4 - len) << 3));
+  }
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int a = is_mmio(addr) ;
+  if(a == -1)
+  {
+  		memcpy(guest_to_host(addr), &data, len);
+  }
+  else mmio_write(addr, len, data, a);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
